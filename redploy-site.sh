d72849a5b16927a110b701880d578dd5fd01e81c
samtuhseng <samantha.tseng90@gmail.com>
2023-03-26 00:55:04 -0700

    docker

---
+++ redploy-site.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Step: cd into project folder
+cd /path/to/your/project-folder
+
+# Step: Get latest changes from main branch on GitHub
+git fetch && git reset origin/main --hard
+
+# Step: Spin containers down to prevent out of memory issues on the VPS instance
+docker-compose -f docker-compose.prod.yml down
+
+# Step: Build and start containers in detached mode
+docker-compose -f docker-compose.prod.yml up -d --build
